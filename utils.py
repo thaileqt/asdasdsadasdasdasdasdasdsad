554191d996cae7e01025645c37a83b10f9b5e0fe
Thai Le <75875212+thaileqt@users.noreply.github.com>
2023-06-20 01:30:05 +0700

    add some functions for utils.py

--- utils.py
+++ utils.py
@@ -13,8 +13,25 @@ def load_data(folder_path):
     combined_df.drop('index', axis=1, inplace=True)
     return combined_df
 
+def get_id_list_from_df(df, save_to_file=False):
+
+    id_list = df["id"].tolist()
+    # save id_list to file
+    if save_to_file:
+        with open("id_list4.txt", "w") as f:
+            for id in id_list:
+                f.write(str(id) + "\n")
+    return id_list
+
+
+def get_id_list_from_file(file_path):
+    with open(file_path, "r") as f:
+        id_list = f.read().splitlines()
+    return id_list
+
 
 if __name__ == "__main__":
-    folder_path = "data3"
+    folder_path = "data4"
     df = load_data(folder_path)
-    print(df.info())
+    get_id_list_from_df(df, save_to_file=True)
+    print(len(get_id_list_from_file("id_list4.txt")))
